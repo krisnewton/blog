b093f1367b0e81ad0de2e9d3fa8547048f6ad875
harishariyanto <66108491+harishariyanto@users.noreply.github.com>
2020-09-03 18:36:49 +0700

    Perbaikan

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,22 @@
 <?php
 
 Route::group(['prefix' => 'blog', 'as' => 'blog.', 'middleware' => 'web'], function () {
-	Route::get('/', 'Apps\BlogController@index')->name('home');
+	Route::get('/', 'App\Http\Controllers\Apps\BlogController@index')->name('home');
 
 	Route::permanentRedirect('post', '/');
-	Route::get('post/{post}', 'Apps\BlogController@post')->name('post');
+	Route::get('post/{post}', 'App\Http\Controllers\Apps\BlogController@post')->name('post');
 
 	Route::permanentRedirect('category', '/');
-	Route::get('category/{category}', 'Apps\BlogController@category')->name('category');
+	Route::get('category/{category}', 'App\Http\Controllers\Apps\BlogController@category')->name('category');
 
 	Route::permanentRedirect('label', '/');
-	Route::get('label/{label}', 'Apps\BlogController@label')->name('label');
+	Route::get('label/{label}', 'App\Http\Controllers\Apps\BlogController@label')->name('label');
 
-	Route::get('search', 'Apps\BlogController@search')->name('search');
+	Route::get('search', 'App\Http\Controllers\Apps\BlogController@search')->name('search');
 });
 
 Route::group(['middleware' => 'web'], function () {
-	Route::get('posts/datatables', 'Apps\PostController@datatables')->name('posts.datatables');
-	Route::resource('posts', 'Apps\PostController')->except(['show']);
-	Route::resource('categories', 'Apps\CategoryController')->except(['show']);
+	Route::get('posts/datatables', 'App\Http\Controllers\Apps\PostController@datatables')->name('posts.datatables');
+	Route::resource('posts', 'App\Http\Controllers\Apps\PostController')->except(['show']);
+	Route::resource('categories', 'App\Http\Controllers\Apps\CategoryController')->except(['show']);
 });
